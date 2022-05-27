810b61c4b70cb4b01b5a5370cbd6123dbae6ee06
Muhammad Zeeshan <ezeeshanpk@gmail.com>
2022-05-27 20:31:03 +0500

    first commit for seafood

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Muhammad Zeeshan on 16/04/2022.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
